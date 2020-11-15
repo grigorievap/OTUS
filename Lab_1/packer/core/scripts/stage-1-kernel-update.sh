3f89955587885f57ef31c9387e944e5b9460fdc8
Klaus <grigorievap@ya.ru>
2020-11-15 22:00:27 +0300

    lab1

--- Lab_1/packer/core/scripts/stage-1-kernel-update.sh
+++ Lab_1/packer/core/scripts/stage-1-kernel-update.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-#Install prerequisites for build kernel from source
-yum install -y gcc bc wget ncurses-devel bison flex elfutils-libelf-devel openssl-devel
+#Install prerequisites
+yum update -y
+yum groupinstall -y "Development Tools"
+
+yum install -y gcc bc wget ncurses-devel bison flex elfutils-libelf-devel openssl-devel devtoolset-8
+source /opt/rh/devtoolset-8/enable
+
+#yum install -y yum kernel-devel kernel-headers rpm-build centos-release-scl
 cd ~
-wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.4.tar.xz
-tar -xf linux-5.4.tar.xz
-cd linux-5.4
+
+wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.9.3.tar.xz 
+tar -xf linux-5.9.3.tar.xz 
+rm -r linux-5.9.3.tar.xz 
+cd linux-5.9.3
 cp -v /boot/config-$(uname -r) .config
-sudo make oldconfig && sudo make -j $(nproc) && sudo make modules_install
+yes "" | make oldconfig
+sudo make -j $(nproc) && sudo make modules_install
 
 # Remove older kernels (Only for demo! Not Production!)
 rm -f /boot/*3.10*
