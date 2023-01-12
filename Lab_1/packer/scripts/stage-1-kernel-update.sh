a1da4524ddf168a66e878263bb36d79e8dd7b8db
Klaus <grigorievap@ya.ru>
2023-01-12 23:26:18 +0300

    1 лаба

--- Lab_1/packer/scripts/stage-1-kernel-update.sh
+++ Lab_1/packer/scripts/stage-1-kernel-update.sh
@@ -4,12 +4,12 @@
 sudo yum install -y https://www.elrepo.org/elrepo-release-8.el8.elrepo.noarch.rpm
 
 # Установка нового ядра из репозитория elrepo-kernel
-yum --enablerepo elrepo-kernel install kernel-ml -y
+sudo yum --enablerepo elrepo-kernel install kernel-ml -y
 
 # Обновление параметров GRUB
-grub2-mkconfig -o /boot/grub2/grub.cfg
-grub2-set-default 0
+sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+sudo grub2-set-default 0
 echo "Grub update done."
 
 # Перезагрузка ВМ
-shutdown -r now
+sudo shutdown -r now
--- README.md
+++ README.md
@@ -7,5 +7,5 @@
 Хост - Windows 11
 Vagrant 2.3.4
 Packer 1.8.5
-VirtualBox - Версия 7.0.4
+VirtualBox - Версия 6.1.40
 ```
